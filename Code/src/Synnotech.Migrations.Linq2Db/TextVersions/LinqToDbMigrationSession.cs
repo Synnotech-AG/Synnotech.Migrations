d56f77e9dc0a6b5e8545d41baae6267c627cd47c
Kenny Pflug <kenny.pflug@live.de>
2021-05-31 19:33:25 +0200

    created generic version for LinqToDbMigrationSession

--- Code/src/Synnotech.Migrations.Linq2Db/TextVersions/LinqToDbMigrationSession.cs
+++ Code/src/Synnotech.Migrations.Linq2Db/TextVersions/LinqToDbMigrationSession.cs
@@ -5,14 +5,74 @@ using LinqToDB;
 using LinqToDB.Data;
 using Synnotech.Linq2Db;
 using Synnotech.Migrations.Core;
+using Synnotech.Migrations.Core.TextVersions;
 
 namespace Synnotech.Migrations.Linq2Db.TextVersions
 {
     /// <summary>
+    /// Represents the session that is used to apply migrations and store corresponding migration info via LinqToDB.
+    /// </summary>
+    /// <typeparam name="TDataConnection">Your custom subtype that derives from <see cref="DataConnection"/>.</typeparam>
+    /// <typeparam name="TMigrationInfo">The type that represents a migration info. It must derive from <see cref="BaseMigrationInfo" />.</typeparam>
+    public class LinqToDbMigrationSession<TDataConnection, TMigrationInfo> : AsyncReadOnlySession<TDataConnection>, IMigrationSession<TDataConnection, TMigrationInfo>
+        where TMigrationInfo : BaseMigrationInfo
+        where TDataConnection : DataConnection
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="LinqToDbMigrationSession{TDataConnection, TMigrationInfo}"/>.
+        /// </summary>
+        /// <param name="dataConnection">The LinqToDB data connection used to interact with the database.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="dataConnection" /> is null.</exception>
+        public LinqToDbMigrationSession(TDataConnection dataConnection) : base(dataConnection) { }
+
+        /// <summary>
+        /// Commits the underlying transaction, if there is any.
+        /// </summary>
+        public Task SaveChangesAsync(CancellationToken cancellationToken = default) =>
+            DataConnection.CommitTransactionAsync(cancellationToken);
+
+        /// <summary>
+        /// Returns the underlying LinqToDB data connection that is used as the context for migrations.
+        /// </summary>
+        public TDataConnection Context => DataConnection;
+
+        /// <summary>
+        /// Inserts the specified migration info into the database. If the database is configured to generate
+        /// an ID for the migration info when this statement is executed, it will not be written back to the
+        /// migration info entity.
+        /// </summary>
+        public virtual ValueTask StoreMigrationInfoAsync(TMigrationInfo migrationInfo, CancellationToken cancellationToken = default) =>
+            new (DataConnection.InsertAsync(migrationInfo, token: cancellationToken));
+    }
+
+    /// <summary>
+    /// Represents the session that is used to apply migrations and store corresponding migration info via LinqToDB.
+    /// <see cref="MigrationInfo" /> is used as the type that represents a migration info.
+    /// </summary>
+    /// <typeparam name="TDataConnection">Your custom subtype that derives from <see cref="DataConnection"/>.</typeparam>
+    public class LinqToDbMigrationSession<TDataConnection> : LinqToDbMigrationSession<TDataConnection, MigrationInfo>
+        where TDataConnection : DataConnection
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="LinqToDbMigrationSession{TDataConnection}"/>.
+        /// </summary>
+        /// <param name="dataConnection">The LinqToDB data connection used to interact with the database.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="dataConnection" /> is null.</exception>
+        public LinqToDbMigrationSession(TDataConnection dataConnection) : base(dataConnection) { }
+
+        /// <summary>
+        /// Stores the specified migration info in the database. The generated ID is written back to the Id property
+        /// of the migration info object.
+        /// </summary>
+        public override async ValueTask StoreMigrationInfoAsync(MigrationInfo migrationInfo, CancellationToken cancellationToken = default) =>
+            migrationInfo.Id = await DataConnection.InsertWithInt32IdentityAsync(migrationInfo, token: cancellationToken);
+    }
+
+    /// <summary>
     /// Represents the session that is used to apply a migration and store the corresponding migration info.
     /// <see cref="MigrationInfo" /> is used as the type that represents a migration info.
     /// </summary>
-    public sealed class LinqToDbMigrationSession : AsyncReadOnlySession, IMigrationSession<DataConnection, MigrationInfo>
+    public class LinqToDbMigrationSession : LinqToDbMigrationSession<DataConnection>
     {
         /// <summary>
         /// Initializes a new instance of <see cref="LinqToDbMigrationSession" />.
@@ -20,23 +80,5 @@ namespace Synnotech.Migrations.Linq2Db.TextVersions
         /// <param name="dataConnection">The LinqToDB data connection used to interact with the database.</param>
         /// <exception cref="ArgumentNullException">Thrown when <paramref name="dataConnection" /> is null.</exception>
         public LinqToDbMigrationSession(DataConnection dataConnection) : base(dataConnection) { }
-
-        /// <summary>
-        /// Returns the underlying LinqToDB <see cref="DataConnection" /> that is used as the context
-        /// for migrations.
-        /// </summary>
-        public DataConnection Context => DataConnection;
-
-        /// <summary>
-        /// Stores the specified migration info in the database.
-        /// </summary>
-        public async ValueTask StoreMigrationInfoAsync(MigrationInfo migrationInfo, CancellationToken cancellationToken = default) =>
-            migrationInfo.Id = await DataConnection.InsertWithInt32IdentityAsync(migrationInfo, token: cancellationToken);
-
-        /// <summary>
-        /// Commits the underlying transaction to the database (if there is any).
-        /// </summary>
-        public Task SaveChangesAsync(CancellationToken cancellationToken = default) =>
-            DataConnection.CommitTransactionAsync(cancellationToken);
     }
 }
