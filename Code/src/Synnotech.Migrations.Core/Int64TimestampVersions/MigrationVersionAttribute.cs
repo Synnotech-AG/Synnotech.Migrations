b26bbe2a418580b901a5e50d85e6a0aeb97257a1
Kenny Pflug <kenny.pflug@live.de>
2021-09-07 16:28:30 +0200

    implemented TimestampParser
    tests are still missing

--- Code/src/Synnotech.Migrations.Core/Int64TimestampVersions/MigrationVersionAttribute.cs
+++ Code/src/Synnotech.Migrations.Core/Int64TimestampVersions/MigrationVersionAttribute.cs
@@ -1,30 +1,54 @@
 ﻿using System;
+using Light.GuardClauses.FrameworkExtensions;
 
 namespace Synnotech.Migrations.Core.Int64TimestampVersions
 {
     /// <summary>
     /// Represents the version of a migration that can be obtained via reflection.
-    /// The version is a <see cref="long" /> value that represents a timestamp.
+    /// The version is a <see cref="long" /> value that should represent a timestamp.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
     public sealed class MigrationVersionAttribute : Attribute, IMigrationAttribute, IHasMigrationVersion<long>
     {
         /// <summary>
-        /// Initializes a new instance of <see cref="MigrationVersionAttribute" />.
+        /// Initializes a new instance of <see cref="MigrationVersionAttribute" />. With this constructor,
+        /// you can pass in the Int64 value directly. We recommend you use the other constructor that
+        /// parses an ISO 8601 UTC timestamp string to avoid malformed version values.
         /// </summary>
         /// <param name="version">The <see cref="long" /> timestamp that uniquely identifies the migration.</param>
         public MigrationVersionAttribute(long version) => Version = version;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="MigrationVersionAttribute"/>. You should prefer this
+        /// constructor as the specified string is parsed as an ISO 8601 UTC timestamp, which avoids malformed
+        /// versions.
+        /// </summary>
+        /// <param name="iso8601UtcTimestamp">
+        /// A string that represents
+        /// </param>
+        public MigrationVersionAttribute(string iso8601UtcTimestamp)
+        {
+            Iso8601UtcTimestamp = iso8601UtcTimestamp;
+            if (TimestampParser.TryParseTimestamp(iso8601UtcTimestamp, out var version))
+                Version = version;
+        }
 
         /// <summary>
         /// Gets the version of the migration.
         /// </summary>
         public long Version { get; }
 
+        private string? Iso8601UtcTimestamp { get; }
+
         long IHasMigrationVersion<long>.GetMigrationVersion() => Version;
 
         /// <summary>
-        /// Does nothing - the version cannot be parsed in a wrong manner currently.
+        /// Checks if a possible ISO 8601 UTC timestamp is valid, or throws a <see cref="MigrationException" /> otherwise.
         /// </summary>
-        public void Validate(Type migrationType) { }
+        public void Validate(Type migrationType)
+        {
+            if (Iso8601UtcTimestamp != null && Version == 0L)
+                throw new MigrationException($"The specified version \"{Iso8601UtcTimestamp}\" of migration {migrationType.ToStringOrNull()} cannot be parsed");
+        }
     }
 }
--- Code/tests/Synnotech.Migrations.Core.Tests/Int64TimestampVersions/MigrationParserTests.cs
+++ Code/tests/Synnotech.Migrations.Core.Tests/Int64TimestampVersions/MigrationParserTests.cs
@@ -19,7 +19,8 @@ namespace Synnotech.Migrations.Core.Tests.Int64TimestampVersions
         public static readonly TheoryData<string, DateTime> Iso8601Data =
             new ()
             {
-                { "2021-09-06T16:45:00Z", new DateTime(2021, 9, 6, 16, 45, 0, DateTimeKind.Utc) }
+                { "2021-09-06T16:45Z", new DateTime(2021, 9, 6, 16, 45, 0, DateTimeKind.Utc) },
+                { "1987-02-12T10:32:56Z", new DateTime(1987, 2, 12, 10, 32, 56, DateTimeKind.Utc) }
             };
     }
 }
