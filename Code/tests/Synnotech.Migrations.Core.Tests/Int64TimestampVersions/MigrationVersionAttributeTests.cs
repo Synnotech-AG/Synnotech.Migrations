a51677ef9fdc5c4ab1e532ea9cce9048ff03b94d
Kenny Pflug <kenny.pflug@live.de>
2021-09-08 08:07:11 +0200

    added tests for migration version that test parsing ISO 8601 strings

--- Code/tests/Synnotech.Migrations.Core.Tests/Int64TimestampVersions/MigrationVersionAttributeTests.cs
+++ Code/tests/Synnotech.Migrations.Core.Tests/Int64TimestampVersions/MigrationVersionAttributeTests.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using FluentAssertions;
 using Synnotech.Migrations.Core.Int64TimestampVersions;
+using Synnotech.Xunit;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Synnotech.Migrations.Core.Tests.Int64TimestampVersions
 {
-    public static class MigrationVersionAttributeTests
+    public sealed class MigrationVersionAttributeTests
     {
+        public MigrationVersionAttributeTests(ITestOutputHelper output) => Output = output;
+
+        private ITestOutputHelper Output { get; }
+
         [Fact]
         public static void MustDeriveFromAttribute() =>
             typeof(MigrationVersionAttribute).Should().BeDerivedFrom<Attribute>();
@@ -25,6 +31,30 @@ namespace Synnotech.Migrations.Core.Tests.Int64TimestampVersions
             attribute.Version.Should().Be(version);
             Action validate = () => attribute.Validate(typeof(MigrationVersionAttributeTests));
             validate.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData("2021-09-08T08:01Z", 20210908080100L)]
+        [InlineData("2018-02-28T19:31:48Z", 20180228193148L)]
+        public static void ValidIso8601TimestampShouldNotThrow(string iso8601Timestamp, long expectedVersion)
+        {
+            var attribute = new MigrationVersionAttribute(iso8601Timestamp);
+
+            attribute.Version.Should().Be(expectedVersion);
+            Action act = () => attribute.Validate(typeof(MigrationVersionAttributeTests));
+            act.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData("Foo")]
+        [InlineData("2021-49-27T58:02Z")]
+        public void InvalidIso8601TimestampShouldThrowOnValidate(string invalidTimestamp)
+        {
+            var attribute = new MigrationVersionAttribute(invalidTimestamp);
+
+            Action act = () => attribute.Validate(typeof(MigrationVersionAttributeTests));
+
+            act.Should().Throw<MigrationException>().Which.ShouldBeWrittenTo(Output);
         }
     }
 }
