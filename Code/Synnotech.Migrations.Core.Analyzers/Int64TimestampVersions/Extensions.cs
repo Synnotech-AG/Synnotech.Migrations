54a866c193bcf7959158f5162810a05c65250a86
Kenny Pflug <kenny.pflug@live.de>
2021-09-09 17:30:32 +0200

    started version attribute code fix

---
+++ Code/Synnotech.Migrations.Core.Analyzers/Int64TimestampVersions/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Synnotech.Migrations.Core.Analyzers.Int64TimestampVersions
+{
+    public static class Extensions
+    {
+        public static bool IsInt64TimestampMigration(this INamedTypeSymbol typeSymbol)
+        {
+            if (typeSymbol.TypeKind != TypeKind.Class)
+                return false;
+
+
+        }
+    }
+}
---
+++ Code/Synnotech.Migrations.Core.Analyzers/Int64TimestampVersions/VersionAttributeAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Synnotech.Migrations.Core.Analyzers.Int64TimestampVersions
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MigrationVersionAttributeAnalyzer : DiagnosticAnalyzer
+    {
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.EnableConcurrentExecution();
+            context.RegisterSymbolAction(AnalyzeMigrationClassForVersionAttribute, SymbolKind.NamedType);
+        }
+
+        private void AnalyzeMigrationClassForVersionAttribute(SymbolAnalysisContext context)
+        {
+            var typeSymbol = (INamedTypeSymbol) context.Symbol;
+
+            if (!typeSymbol.IsInt64TimestampMigration())
+                return;
+        }
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+    }
+}
